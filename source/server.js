b7d487eb4a5870c11ecae8e52e2241d61439a81e
Samandarbek Samiyev <samandarbeksamiyev1999@gmail.com>
2022-06-07 13:01:28 +0200

    Sem

--- source/server.js
+++ source/server.js
@@ -1,25 +1,25 @@
-import express from 'express'
-import './config.js'
-import UserRouter from './routers/index.js'
-import database from './db/index.js'
-import mockData from './db/mock.js'
+// import express from 'express'
+// import './config.js'
+// import UserRouter from './routers/index.js'
+// import database from './db/index.js'
+// import mockData from './db/mock.js'
 
-const PORT = process.env.PORT || 6000
+// const PORT = process.env.PORT || 6000
 
-!async function () {
-    const app = express()
+// !async function () {
+//     const app = express()
 
     
-    const db = await database()
-    await mockData({ sequelize: db })
+//     const db = await database()
+//     await mockData({ sequelize: db })
     
-    app.use(express.json())
-    app.use((req, res, next) => {
-        req.models = db.models
-        next()
-    })
+//     app.use(express.json())
+//     app.use((req, res, next) => {
+//         req.models = db.models
+//         next()
+//     })
     
-    app.use(UserRouter)
+//     app.use(UserRouter)
     
-    app.listen(PORT, () => console.log('server ready at *' + PORT))
-}()
+//     app.listen(PORT, () => console.log('server ready at *' + PORT))
+// }()
